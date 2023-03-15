e551f288756944bfa2974436b360fdf85be79796
WiiCoder4444python <127691212+WiiCoder4444python@users.noreply.github.com>
2023-03-15 12:13:43 +0000

    Update timer2.0.py

--- timer2.0.py
+++ timer2.0.py
@@ -4,7 +4,7 @@ import time
 import math
 #sets up sounds
 pygame.mixer.init()
-pygame.mixer.music.load('/home/pi/Documents/Sound Python WAVs/mario64complete.wav')
+pygame.mixer.music.load('~/Documents/Sound Python WAVs/mario64complete.wav')
 #timer function
 def timer():
     #sets variables
